6240060d1a980bd23df9b560711b880154f4d33a
Yilberth Andres <89434567+YilberthAndres@users.noreply.github.com>
2023-02-19 17:15:07 -0500

    traer permisos para creacion

--- src/infrastructure/controller/role/role.controller.ts
+++ src/infrastructure/controller/role/role.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
 import { RolesUsesCases } from "../../../aplication/roles/roles.usesCases";
+import { PermissionsUsesCases } from "../../../aplication/permissions/permissions.usesCases";
 
 export class RolesController {
-  constructor(private rolesUsesCases: RolesUsesCases) {}
+  constructor(
+    private rolesUsesCases: RolesUsesCases,
+    private permissionsUsesCases: PermissionsUsesCases
+    ) {}
 
   GetAll = async (req: Request, res: Response) => {
     const resp = await this.rolesUsesCases.listRoles();
@@ -12,7 +16,7 @@ export class RolesController {
   GetCreateRole = async (req: Request, res: Response) => {
     // createRoles
 
-    const resp = await this.rolesUsesCases.createRoles()
+    const resp = await this.permissionsUsesCases.listPermissions()
     return res.json(resp);
   }
 }
--- src/infrastructure/routes/role/role.route.ts
+++ src/infrastructure/routes/role/role.route.ts
@@ -4,12 +4,18 @@ const route = Router();
 
 import { RolesController } from "../../controller/role/role.controller";
 import { RolesRepositoryClass } from "../../repository/rolesRepository/rolesRepository";
+import { PermissionsRepositoryClass } from "../../repository/permissionsRepository/permissionsRepository";
 import { RolesUsesCases } from "../../../aplication/roles/roles.usesCases";
+import { PermissionsUsesCases } from "../../../aplication/permissions/permissions.usesCases";
 
 /** Injeciones de dependencia*/
 const rolesRepository = new RolesRepositoryClass();
+const permissionsRepository = new PermissionsRepositoryClass();
+
 const rolesUsescases = new RolesUsesCases(rolesRepository);
-const rolesController = new RolesController(rolesUsescases);
+const permissionsUsescases = new PermissionsUsesCases(permissionsRepository);
+
+const rolesController = new RolesController(rolesUsescases,permissionsUsescases );
 
 /** Rutas para la aplicaion */
 
