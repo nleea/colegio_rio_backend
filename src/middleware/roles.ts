4ce4f1bf5cd8973eee5ff37a3585cc3c39a81c24
Yilberth Andres <89434567+YilberthAndres@users.noreply.github.com>
2023-02-21 14:10:06 -0500

    roles

---
+++ src/middleware/roles.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { RolesUsesCases } from "/../riodev/colegio_rio_backend/src/aplication/roles/roles.usesCases"
+import { PermissionsUsesCases } from "/../riodev/colegio_rio_backend/src/aplication/permissions/permissions.usesCases";
+import { RoleCreateEntity } from "/../riodev/colegio_rio_backend/src/domain/roles/roles.entity";
+import { RoleCreateValue } from "/../riodev/colegio_rio_backend/src/domain/roles/roles.value";
+import { UserEntity } from "../domain/user/user.entity";
+import { UserUsesCases } from "../aplication/user/user.usesCases";
+
+
+
+
+export const ValidarPermiso = async (req: Request, res: Response, next: () => void) => {
+  ;
+    try {
+      const token = req.headers.authorization;
+      // console.log(req.headers.authorization)
+  
+      if (!token) return res.status(403).json({ message: "Not token provided" });
+  
+      // const decode = jwt.verify(token, config.SECRET);
+  
+      // const user = await User.findById(decode.id, { password: 0 });
+      const id = req.user! as UserEntity;
+    // const { data, ok, status } = await this.userUsesCases.userProfile(id);
+    // return res.status(status).json({ ok, data });
+  
+      console.log(id.perfil_id )
+    //   if (!user) return res.status("404").json({ message: "User not found" });
+  
+      next();
+    } catch (error) {
+      res.status(404).json({ message: 'Unauthorized' });
+    }
+  };
