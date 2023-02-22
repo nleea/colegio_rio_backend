2f397abf37296cd86a00afc0e2b411fc76db65f0
Yilberth Andres <89434567+YilberthAndres@users.noreply.github.com>
2023-02-21 20:46:57 -0500

    permiso ver roles

--- src/middleware/roles.ts
+++ src/middleware/roles.ts
@@ -13,41 +13,43 @@ export const VerRoles = async (
   next: () => void
 ) => {
   const permissionVer = "ver-rol";
-
   try {
     const token = req.headers.authorization;
-    // console.log(req.headers.authorization)
-
     if (!token) return res.status(403).json({ message: "Not token provided" });
 
     const id = req.user! as UserEntity;
 
-    const resp = await db.users.findMany({
-      where: {
-        id: id.id,
-      },
-      include: {roles_users_role_idToroles:{include:{role_has_permissions:{include:{permissions:true}}}}}
-    });
-
-    // if (!resp) return res.status(403).json({ message: "No tienes permisos!!" });
-
-    const permisos = resp[0].roles_users_role_idToroles?.role_has_permissions;
-
-    let tienePermiso = false;
-    permisos?.forEach( role => {
-      // console.log(role)
-
-      if(role.permissions.name == permissionVer){
-        tienePermiso = true;
-      }else{
-        console.log(role.permissions.name + ' - ' + permissionVer)
-      }
-    })
-
-    if (!tienePermiso) return res.status(403).json({ message: "No tienes permisos!!" });
-
+    const user_id = id.role_id;
+    const tienePermiso = await consulta(user_id, permissionVer);
+    if (!tienePermiso)return res.status(403).json({ message: "No tienes permisos!!" });
     next();
   } catch (error) {
     res.status(404).json({ message: "Unauthorized" });
   }
 };
+
+
+export const consulta = async (id:any, permissionVer:String) => {
+
+  const resp = await db.users.findMany({
+    where: {
+      id: id,
+    },
+    include: {
+      roles_users_role_idToroles: {
+        include: { role_has_permissions: { include: { permissions: true } } },
+      },
+    },
+  });
+
+  const permisos = resp[0].roles_users_role_idToroles?.role_has_permissions;
+
+  let tienePermiso = false;
+  permisos?.forEach((role) => {
+    if (role.permissions.name == permissionVer) {
+      tienePermiso = true;
+    } 
+  });
+
+  return tienePermiso;
+}
